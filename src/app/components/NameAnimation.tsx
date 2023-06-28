8f2f3a458d0c0a5a8117b6fc1b781b896cf831d1
mahakanakala <maha.kanakala@gmail.com>
2023-06-28 14:26:58 -0400

    add NameAnimation & refactor components

---
+++ src/app/components/NameAnimation.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import styles from '../page.module.css';
+
+const NameAnimation = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [currentChar, setCurrentChar] = useState("");
+
+  const typingSpeed = 200;
+  const texts = ['maha', 'महा', 'మహా'];
+
+  useEffect(() => {
+    const typingTimer = setTimeout(() => {
+      setInputValue(prevValue => prevValue + currentChar);
+      setCurrentChar("");
+    }, typingSpeed);
+
+    return () => clearTimeout(typingTimer);
+  }, [currentChar]);
+
+  useEffect(() => {
+    const currentText = texts.join("");
+    let currentIndex = 0;
+
+    const typingInterval = setInterval(() => {
+      setCurrentChar(currentText[currentIndex]);
+      currentIndex = (currentIndex + 1) % currentText.length;
+
+      if (currentIndex === 0) {
+        clearInterval(typingInterval);
+      }
+    }, typingSpeed);
+
+    return () => clearInterval(typingInterval);
+  },);
+
+  return <input type="text" className={styles.nameCenter} value={inputValue} />;
+};
+
+export default NameAnimation;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,15 @@
+"use client"
 import React from "react";
-import dynamic from "next/dynamic";
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Playfair_Display } from "next/font/google";
-import { Noto_Sans_Devanagari } from "next/font/google";
-import { Noto_Serif_Telugu } from "next/font/google";
-import { EB_Garamond } from "next/font/google"
-import Typewriter from 'typewriter-effect';
-import { BsArrowUpRight } from 'react-icons/Bs';
+// import NameAnimation from './components/NameAnimation'
 
+import { BsArrowUpRight } from 'react-icons/bs';
 
-const Devarangi = Noto_Sans_Devanagari({ subsets: ["latin"] });
-const Telugu = Noto_Serif_Telugu({ subsets: ["latin"] });
-const Playfair = Playfair_Display({ subsets: ["latin"] });
-
-// const DynamicProjectsSection = dynamic(() => import("./ProjectsSection.js"), {
-//   ssr: false,
-// });
+const typingText = ['maha', 'महा', 'మహా'];
 
 export default function Home() {
-  const selectedPage = "Home";
-
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -32,51 +21,13 @@ export default function Home() {
             <div className={styles.subtitle}>COMPUTER SCIENCE STUDENT</div>
           </div>
         </div>
-        <nav className={styles.navbar}>
-          <ul className={styles.navList}>
-            <li
-              className={`${styles.navItem} ${selectedPage === "Home" ? styles.active : ""
-                }`}
-            >
-              <a href="#">Home</a>
-            </li>
-            <li
-            // className={`${styles.navItem} ${selectedPage === "Projects" ? styles.active : ""
-            //   }`}
-            >
-              <a href="#">Projects</a>
-            </li>
-            <li
-
-            >
-              <a href="#">Blog</a>
-            </li>
-            <li
-
-            >
-              <a href="#">Photos</a>
-            </li>
-            <li
-
-            >
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
       </header>
       <div className={styles.contentContainer}>
         <section className={styles.heroSection}>
           <div className={styles.nameSection}>
             <h1 className={styles.nameCenter}> MAHA
-              {/* <Typewriter
-                options={{
-                  strings: ['Hello', 'World'],
-                  autoStart: true,
-                  loop: true,
-                }}
-              /> */}
-
             </h1>
+            {/* <NameAnimation /> */}
             <div className={styles.floatChildElement}>
               <Image
                 src="/images/mandala.png"
@@ -100,7 +51,7 @@ export default function Home() {
             <div className={styles.projectView}> VIEW PROJECT</div>
             <div className={styles.arrow}>
               <a href="https://github.com/mahakanakala/toxins-in-skincare"><BsArrowUpRight /></a>
-               </div>
+            </div>
           </div>
         </section>
         <section>
@@ -123,13 +74,10 @@ export default function Home() {
             <div className={styles.projectLanguages}> PYTHON </div>
             <div className={styles.projectView}> VIEW PROJECT</div>
             <div className={styles.arrow}>
-
               <BsArrowUpRight />  </div>
           </div>
         </section>
         <hr className={styles.sectionLine} />
-
-        
       </div>
     </div>
   );
