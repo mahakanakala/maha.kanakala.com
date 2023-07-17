0c37aaa94821270101c61c6e5e5de50e8c21a139
mahakanakala <maha.kanakala@gmail.com>
2023-07-17 17:42:05 -0400

    add media queries for desktop & laptop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,10 +50,8 @@ export default function Home() {
         <section className={styles.heroSection}>
           <h3 className={styles.headerIntro}> MAHA KANAKALA</h3>
           <h2 className={styles.openerName}>SOFTWARE ENGINEERING STUDENT</h2>
-          <div className={styles.nameSection}>
             <div className={styles.nameContainer}>
               <h1 className={styles.nameCenter}> MAHA </h1>
-            </div>
             {/* <div className={styles.videoContainer}>
               <video autoPlay muted loop >
                 <source src="/videos/0208d89b4b72440ca37722f78bf78e76.webm" type="video/webm" />
