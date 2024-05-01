ed5b5887af0e65e306e8835da5418dd5a9602304
mahakanakala <maha.kanakala@gmail.com>
2024-05-01 16:20:58 -0400

    add media query for success page

--- src/app/21-bday-success/page.tsx
+++ src/app/21-bday-success/page.tsx
@@ -6,9 +6,9 @@ export default function SuccessPage() {
         <>
             <div className={styles.successMain}>
                 <div className={styles.successContainer}>
-                    <h1 className={styles.dressCodeTitle}>Thank you for your RSVP!</h1>
+                    <h1 className={styles.sucessTitle}>Thank you for your RSVP!</h1>
                     <p className={styles.dressCodeCaption}>We look forward to celebrating with you. In the mean time you can add to the playlist!</p>
-                    <iframe className={styles.spotifyContainer} src="https://open.spotify.com/embed/playlist/74F1rWeTCsvvH0slYmVewG?utm_source=generator" width="100%" height="352" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe className={styles.spotifyContainer} src="https://open.spotify.com/embed/playlist/74F1rWeTCsvvH0slYmVewG?utm_source=generator" width="100%" height="352" frameBorder="0"  allow="autoplay;  clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </div>
             </div>
         </>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           </ul>
         </nav> */}
         <RestrictRotation />
-        { <Footer />}
+        {/* { <Footer />} */}
       </body>
     </html>
   )
